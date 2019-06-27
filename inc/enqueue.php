eac2be14e9a6ab84ba72957da7d35d9b75f8831e
AndreasLF <andreas@lyhnefiehn.dk>
2019-06-27 14:11:02 +0200

    + Added bootstrap framwork + Created custom JavaScript file + Created header and footer documents + Created navbar example

---
+++ inc/enqueue.php
@@ -0,0 +1,35 @@
+<?php
+
+/* Enqueues and registers all stylesheets and scripts */
+function ap_enqueue(){
+
+
+    /*--------------------*\
+            Styles
+    \*--------------------*/
+    //Register styles
+    wp_register_style('ap_style', get_stylesheet_uri(), array('ap_bootstrap_css'));
+    wp_register_style('ap_bootstrap_css', get_template_directory_uri()."/lib/bootstrap-4.3.1-dist/css/bootstrap.min.css");
+    wp_register_style('ap_fontawesome', "https://use.fontawesome.com/releases/v5.1.0/css/all.css",array('mt_style'));
+
+    // Enqueue styles
+    wp_enqueue_style('ap_style');
+    wp_enqueue_style('ap_bootstrap_css');
+    wp_enqueue_style('ap_fontawesome');
+
+
+    /*--------------------*\
+            Scripts
+    \*--------------------*/
+    wp_enqueue_script('jquery');
+    wp_register_script('ap_bootstrap_js', get_template_directory_uri()."/lib/bootstrap-4.3.1-dist/js/bootstrap.min.js",array('jquery'));
+    wp_register_script('ap_script', get_template_directory_uri()."/js/script.js",array('jquery','ap_bootstrap_js'));
+
+    wp_enqueue_script('ap_bootstrap_js');
+    wp_enqueue_script('ap_script');
+
+
+}
+
+
+ ?>
