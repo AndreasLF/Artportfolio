eb4dce9ffc3ec5e726ffad7b6c32f7cd97c6e50f
AndreasLF <andreas@lyhnefiehn.dk>
2019-07-07 14:26:29 +0200

    Remake of the gallery - Simpler grid structure

--- index.php
+++ index.php
@@ -9,87 +9,47 @@
 /*
 * This function creates a photo gallery from an array of photo sources
 *
-* @param array $imageSources array of imgage source links
+* @param array $imageSources array of image source links
 * @return html photo gallery
 */
-
 function ap_create_gallery($imageSources = null){
-  //if $imageSources exists
-   if($imageSources){ ?>
-     <!-- Creation of html-code -->
-         <div class="gallery-row">
-           <div class="gallery-column">
-             <?php
-              //loops through every fourth photo starting with number 0
-               for($i=0; $i<count($imageSources); $i+=4){
-                 ?>
-                 <img src="<?php echo $imageSources[$i]; ?>" />1
-                 <?php
-               }?>
-           </div>
-           <div class="gallery-column">
-             <?php
-             //loops through every fourth photo starting with number 1
-               for($i=1; $i<count($imageSources); $i+=4){
-                 ?>
-                 <img  src="<?php echo $imageSources[$i]; ?>" />2
-                 <?php
-               }
-               ?>
-           </div>
-           <div class="gallery-column">
-             <?php
-             //loops through every fourth photo starting with number 2
-               for($i=2; $i<count($imageSources); $i+=4){
-                 ?>
-                 <img  src="<?php echo $imageSources[$i]; ?>" />3
-                 <?php
-               }
-               ?>
-           </div>
-           <div class="gallery-column">
-             <?php
-             //loops through every fourth photo starting with number 3
-               for($i=3; $i<count($imageSources); $i+=4){
-                 ?>
-                 <img src="<?php echo $imageSources[$i]; ?>" />4
-                 <?php
-               }
-               ?>
-           </div>
-         </div>
-       <?php
-   }
-   else{
-     return;
-   }
+  if($imageSources){
+    ?>
+    <div class="ap-grid">
+    <?php
+      foreach($imageSources as $image){
+        ?>
+        <div class="ap-gallery-block">
+          <img src="<?php echo $image; ?>">
+        </div>
+        <?php
+      }
+    ?>
+    </div>
+    <?php
+  }
+  else {
+    return;
+  }
+}?>
 
-
-} ?>
 
 <!--Container -->
 <div class="container">
   <h2><?php wp_title(''); ?></h2>
     <?php
     if(have_posts()){
-
         $imgSrcs = array();
-                while(have_posts()){
-                    the_post();
-                    $imgSrc = get_post_meta( get_the_ID(), 'image' )[0];
-                    array_push($imgSrcs,$imgSrc);
-?>
 
+        while(have_posts()){
+            the_post();
+            $imgSrc = get_post_meta( get_the_ID(), 'image' )[0];
+            array_push($imgSrcs,$imgSrc);
+        }
 
-<?php
-                }
+        ap_create_gallery($imgSrcs);
 
-                ap_create_gallery($imgSrcs);
-
-            } ?>
-
-</div>
-<!-- /container -->
-
+      } ?>
+    </div><!-- /container -->
 
 <?php get_footer(); ?>
