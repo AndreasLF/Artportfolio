d46db3337b781941c0c3b5b1bad53a6a6119c632
AndreasLF <andreas@lyhnefiehn.dk>
2019-07-23 12:32:18 +0200

    Page layout created

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<!--Container -->
+<div class="container">
+    <?php
+    if(have_posts()){
+        while(have_posts()){
+            the_post();
+            ?>
+            <h2> <?php the_title(); ?></h2>
+            <br />
+            <?php
+            the_content();
+        }
+      } ?>
+    </div><!-- /container -->
+
+
+
+<?php get_footer(); ?>
